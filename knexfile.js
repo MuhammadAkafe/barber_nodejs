e3e2392bde71c96b1f150a4f3aac38ebdffd8b4b
muhammad <allappsdata97@gmail.com>
2025-02-03 02:08:02 +0200

    شششششششش

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: process.env.DB_HOST || '127.0.0.1',
+      host: '127.0.0.1',
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
